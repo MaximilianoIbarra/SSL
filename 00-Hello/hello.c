4ddb607092abacad19c131e34b71a74313b9539d
MaximilianoIbarra <maximilianoibarra@icloud.com>
2018-04-02 12:32:21 -0300

    Add files via upload

---
+++ 00-Hello/hello.c
@@ -0,0 +1,13 @@
+/* Hello.c
+* Escribe por pantalla Hello World!
+* Maximiliano Ibarra
+* Cuando: Fecha de última modificación
+*/
+# include<stdio.h>
+
+int main(){
+	printf("Hello World!\n");
+}
+
+
+
